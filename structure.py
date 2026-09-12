45de7da7bc7a73ec440b6dbdd841e969a7acecf4
DHRUVRANKA <rankadhruv25@gmail.com>
2026-09-12 09:16:46 +0530

    Add files via upload

---
+++ structure.py
@@ -0,0 +1,15 @@
+# Import modules
+import module_name
+
+# Variables / constants
+x = 10
+
+# Functions
+def function_name():
+    # code
+    pass
+
+# Main program
+if __name__ == "__main__":
+    # code to execute
+    pass
